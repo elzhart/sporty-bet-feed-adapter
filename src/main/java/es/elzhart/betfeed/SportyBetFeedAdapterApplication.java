f37c77ba4b6e7cc06366f9281613ed06355e834e
Artur Elzhanov <aelzhanov@amelco.co.uk>
2025-08-20 17:22:44 +0200

    SBFA-1 create gradle project

---
+++ src/main/java/es/elzhart/betfeed/SportyBetFeedAdapterApplication.java
@@ -0,0 +1,11 @@
+package es.elzhart.betfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportyBetFeedAdapterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SportyBetFeedAdapterApplication.class, args);
+    }
+}
---
+++ src/test/java/es/elzhart/betfeed/SportyBetFeedAdapterApplicationTest.java
@@ -0,0 +1,10 @@
+package es.elzhart.betfeed;
+
+import org.springframework.boot.SpringApplication;
+
+public class SportyBetFeedAdapterApplicationTest {
+
+    public static void main(String[] args) {
+        SpringApplication.from(SportyBetFeedAdapterApplication::main).run(args);
+    }
+}
